11de3939b832c62c1486b924f7d6e8f50a7a79f2
Leonardo Delmiro <leodelmiroms@gmail.com>
2020-11-29 22:56:22 -0300

    Feign client

--- hr-payroll/src/main/java/com/leodelmiro/hrpayroll/HrPayrollApplication.java
+++ hr-payroll/src/main/java/com/leodelmiro/hrpayroll/HrPayrollApplication.java
@@ -2,7 +2,9 @@ package com.leodelmiro.hrpayroll;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
+@EnableFeignClients
 @SpringBootApplication
 public class HrPayrollApplication {
 
